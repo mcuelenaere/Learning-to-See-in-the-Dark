debec606bcb2e0081e3160269d37bbb282782903
Maurus Cuelenaere <mcuelenaere@gmail.com>
2020-05-03 17:22:54 +0200

    Rework test_Sony.py to work without ground truth images

--- test_Sony.py
+++ test_Sony.py
@@ -8,19 +8,9 @@ import numpy as np
 import rawpy
 import glob
 
-input_dir = './dataset/Sony/short/'
-gt_dir = './dataset/Sony/long/'
+input_dir = './inputs_Sony/'
 checkpoint_dir = './checkpoint/Sony/'
 result_dir = './result_Sony/'
-
-# get test IDs
-test_fns = glob.glob(gt_dir + '/1*.ARW')
-test_ids = [int(os.path.basename(test_fn)[0:5]) for test_fn in test_fns]
-
-DEBUG = 0
-if DEBUG == 1:
-    save_freq = 2
-    test_ids = test_ids[0:5]
 
 
 def lrelu(x):
@@ -111,45 +101,25 @@ if ckpt:
 if not os.path.isdir(result_dir + 'final/'):
     os.makedirs(result_dir + 'final/')
 
-for test_id in test_ids:
-    # test the first image in each sequence
-    in_files = glob.glob(input_dir + '%05d_00*.ARW' % test_id)
-    for k in range(len(in_files)):
-        in_path = in_files[k]
-        in_fn = os.path.basename(in_path)
-        print(in_fn)
-        gt_files = glob.glob(gt_dir + '%05d_00*.ARW' % test_id)
-        gt_path = gt_files[0]
-        gt_fn = os.path.basename(gt_path)
-        in_exposure = float(in_fn[9:-5])
-        gt_exposure = float(gt_fn[9:-5])
-        ratio = min(gt_exposure / in_exposure, 300)
+in_files = glob.glob(input_dir + '*.ARW')
+for k in range(len(in_files)):
+    in_path = in_files[k]
+    in_name = os.path.basename(in_path).replace('.ARW', '')
+    raw = rawpy.imread(in_path)
+    input_full = np.expand_dims(pack_raw(raw), axis=0)
 
-        raw = rawpy.imread(in_path)
-        input_full = np.expand_dims(pack_raw(raw), axis=0) * ratio
+    im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+    scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
 
-        im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        # scale_full = np.expand_dims(np.float32(im/65535.0),axis = 0)*ratio
-        scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+    input_full = np.minimum(input_full, 1.0)
 
-        gt_raw = rawpy.imread(gt_path)
-        im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
-        gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+    output = sess.run(out_image, feed_dict={in_image: input_full})
+    output = np.minimum(np.maximum(output, 0), 1)
 
-        input_full = np.minimum(input_full, 1.0)
+    output = output[0, :, :, :]
+    scale_full = scale_full[0, :, :, :]
 
-        output = sess.run(out_image, feed_dict={in_image: input_full})
-        output = np.minimum(np.maximum(output, 0), 1)
-
-        output = output[0, :, :, :]
-        gt_full = gt_full[0, :, :, :]
-        scale_full = scale_full[0, :, :, :]
-        scale_full = scale_full * np.mean(gt_full) / np.mean(
-            scale_full)  # scale the low-light image to the same mean of the groundtruth
-
-        scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
-        scipy.misc.toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
-        scipy.misc.toimage(gt_full * 255, high=255, low=0, cmin=0, cmax=255).save(
-            result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
+    scipy.misc.toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
+        result_dir + 'final/%s_out.png' % (in_name))
+    scipy.misc.toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+        result_dir + 'final/%s_scale.png' % (in_name))
